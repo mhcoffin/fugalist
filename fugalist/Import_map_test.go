c574de12039ea18ed9096784c243d799e04b0415
mhc <mhcoffin@gmail.com>
2021-08-13 14:00:09 -0700

    first tests passing

--- fugalist/Import_map_test.go
+++ fugalist/Import_map_test.go
@@ -1,24 +1,223 @@
 package fugalist
 
 import (
+	"encoding/xml"
+	"fmt"
+	"github.com/mhcoffin/go-doricolib/doricolib"
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
 	"testing"
 )
 
+func readExpressionMap(filename string) *doricolib.ExpressionMap {
+	bytes, err := ioutil.ReadFile(filename)
+	if err != nil {
+		panic(fmt.Errorf("failed to read file: %w", err))
+	}
+	scoreLib := &doricolib.ScoreLib{}
+	err = xml.Unmarshal(bytes, scoreLib)
+	if err != nil {
+		panic(fmt.Errorf("failed to unmarshall file (%s): %w", filename, err))
+	}
+	return &scoreLib.ExpressionMaps.Entities.Contents[0]
+}
+
 func TestCanonicalizeTechniqueString(t *testing.T) {
 	tests := []struct {
-		name string
-		orig string
+		name     string
+		orig     string
 		expected string
 	}{
 		{"single", "pt.legato", "pt.legato"},
 		{"sorted", "pt.legato+pt.nonVibrato", "pt.legato+pt.nonVibrato"},
 		{"reversed", "pt.nonVibrato+pt.legato", "pt.legato+pt.nonVibrato"},
-
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			assert.Equal(t, test.expected, CanonicalizeTechniqueString(test.orig))
+		})
+	}
+}
+
+func TestFormatMidiEvents(t *testing.T) {
+	tests := []struct {
+		name     string
+		actions  []doricolib.SwitchAction
+		expected string
+	}{
+		{"empty", []doricolib.SwitchAction{}, ""},
+		{"single KS", []doricolib.SwitchAction{
+			{
+				Type:   "kKeySwitch",
+				Param1: "13",
+			},
+		}, "KS13"},
+		{"single PC", []doricolib.SwitchAction{
+			{
+				Type:   "kProgramChange",
+				Param1: "7",
+			},
+		}, "PC7"},
+		{"single CC", []doricolib.SwitchAction{
+			{
+				Type:   "kControlChange",
+				Param1: "3",
+				Param2: "17",
+			},
+		}, "CC3=17"},
+		{"multiple", []doricolib.SwitchAction{
+			{
+				Type:   "kKeySwitch",
+				Param1: "13",
+			},
+			{
+				Type:   "kProgramChange",
+				Param1: "7",
+			},
+			{
+				Type:   "kControlChange",
+				Param1: "3",
+				Param2: "17",
+			},
+		}, "KS13, PC7, CC3=17"},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			assert.Equal(t, test.expected, FormatMidiEvents(test.actions))
+		})
+	}
+}
+
+func TestFormatMidiDynamic(t *testing.T) {
+	tests := []struct {
+		name     string
+		vtype    doricolib.VolumeType
+		rng      string
+		expected string
+	}{
+		{"velocity full range", doricolib.VolumeType{Type: "kNoteVelocity"}, "0,127", "velocity"},
+		{"velocity part range", doricolib.VolumeType{Type: "kNoteVelocity"}, "10,110", "velocity 10:110"},
+		{"cc full range", doricolib.VolumeType{Type: "kCC", Param1: "13"}, "0,127", "CC13"},
+		{"cc part range", doricolib.VolumeType{Type: "kCC", Param1: "13"}, "10,30", "CC13 10:30"},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			assert.Equal(t, test.expected, FormatMidiDynamic(test.vtype, test.rng))
+		})
+	}
+}
+
+func TestFormatLengthFactor(t *testing.T) {
+	tests := []struct {
+		name     string
+		value    string
+		expected float64
+	}{
+		{"empty", "", 100},
+		{"one", "1.0", 100},
+		{"fraction", "0.85", 85},
+		{"zero", "0.0", 0},
+		{"larger", "1.05", 105},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			assert.Equal(t, test.expected, FormatLengthFactor(test.value))
+		})
+	}
+}
+
+func TestFormatTranspose(t *testing.T) {
+	tests := []struct {
+		name     string
+		value    int
+		expected float64
+	}{
+		{"empty", 0, 0.0},
+		{"empty", 1, 1.0},
+		{"empty", -1, -1.0},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			assert.Equal(t, test.expected, FormatTranspose(test.value))
+
+		})
+	}
+}
+
+func TestFormatBranch(t *testing.T) {
+	tests := []struct {
+		name     string
+		value    string
+		expected string
+	}{
+		{"empty", "", ""},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			assert.Equal(t, test.expected, FormatBranch(test.value))
+		})
+	}
+}
+
+func TestBuildPtMap(t *testing.T) {
+	tests := []struct {
+		name     string
+		expected PtMap
+	}{
+		{
+			name: "Ref",
+			expected: PtMap{
+				"pt.legato": {
+					"NoteLength <= medium": {
+						On:    "KS25, PC6, CC1=64",
+						Dyn:   "velocity 1:127",
+						Len:   100,
+						Trans: 0,
+					},
+					"NoteLength > medium": {
+						On:    "KS26, PC6, CC1=64",
+						Dyn:   "CC2 1:120",
+						Len:   95,
+						Trans: -1,
+					},
+				},
+				"pt.marcato+pt.nonVibrato+pt.plucked": {
+					"": {
+						On:    "KS24, PC13, CC7=23",
+						Dyn:   "velocity 1:127",
+						Len:   100,
+						Trans: 0,
+					},
+				},
+				"pt.natural": {
+					"NoteLength < medium": {
+						On:    "KS12=120, KS24, PC15, CC4=64",
+						Dyn:   "velocity 10:120",
+						Len:   100,
+						Trans: 0,
+					},
+					"NoteLength >= long": {
+						On:    "KS12=120, KS24, PC13, CC4=64",
+						Dyn:   "CC2 10:120",
+						Len:   100,
+						Trans: 0,
+					},
+					"NoteLength >= medium AND NoteLength < long": {
+						On:    "KS12=120, KS24, PC13, CC4=64",
+						Dyn:   "velocity 10:120",
+						Trans: 0,
+						Len:   100,
+					},
+				},
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			xmap := readExpressionMap(fmt.Sprintf("test_input/%s.doricolib", test.name))
+			ptMap, err := BuildPtMap(*xmap)
+			assert.Nil(t, err)
+			assert.Equal(t, test.expected, ptMap)
 		})
 	}
 }
--- fugalist/import_map.go
+++ fugalist/import_map.go
@@ -3,16 +3,17 @@ package fugalist
 import (
 	"fmt"
 	"github.com/mhcoffin/go-doricolib/doricolib"
+	"math"
 	"sort"
 	"strconv"
 	"strings"
 )
 
 type PlayData struct {
-	On string
-	Off string
-	Dyn string
-	Len float64
+	On    string
+	Off   string
+	Dyn   string
+	Len   float64
 	Trans float64
 }
 
@@ -24,23 +25,35 @@ func BuildPtMap(xmap doricolib.ExpressionMap) (PtMap, error) {
 	result := make(PtMap)
 	for _, combo := range xmap.Combinations.Combos {
 		tids := CanonicalizeTechniqueString(combo.TechniqueIDs)
-		cond := combo.ConditionString
-		brmap, exists := result[tids]
-		playData := PlayData {
-			On: FormatMidiEvents(combo.SwitchOnActions.SwitchOnActions),
-			Off: FormatMidiEvents(combo.SwitchOffActions.SwitchOffActions),
-			Dyn: FormatMidiDynamic(combo.VolumeType),
-			Len: FormatLengthFactor(combo.LengthFactor),
+		cond := FormatBranch(combo.ConditionString)
+		playData := PlayData{
+			On:    FormatMidiEvents(combo.SwitchOnActions.SwitchOnActions),
+			Off:   FormatMidiEvents(combo.SwitchOffActions.SwitchOffActions),
+			Dyn:   FormatMidiDynamic(combo.VolumeType, combo.VelocityRange),
+			Len:   FormatLengthFactor(combo.LengthFactor),
 			Trans: FormatTranspose(combo.Transpose),
 		}
+		branchMap, exists := result[tids]
 		if exists {
-			brmap[cond] = PlayData{}
+			branchMap[cond] = playData
 		} else {
 			result[tids] = make(BrMap)
-			result[tids][cond] = PlayData{}
+			result[tids][cond] = playData
 		}
 	}
 	return result, nil
+}
+
+var branchReplacer = strings.NewReplacer(
+	"kVeryShort", "veryShort",
+	"kShort", "short",
+	"kMedium", "medium",
+	"kLong", "long",
+	"kVeryLong", "veryLong",
+)
+
+func FormatBranch(br string) string {
+	return branchReplacer.Replace(br)
 }
 
 func FormatTranspose(transpose int) float64 {
@@ -52,17 +65,42 @@ func FormatLengthFactor(factor string) float64 {
 	if err != nil {
 		return 100.0
 	}
-	return fl * 100.0
+	return math.Round(fl * 100.0)
 }
 
-func FormatMidiDynamic(volumeType doricolib.VolumeType) string {
+func FormatMidiDynamic(volumeType doricolib.VolumeType, vrange string) string {
+	rng := ""
+	parsedRange := strings.Split(vrange, ",")
+	if len(parsedRange) == 2 && (parsedRange[0] != "0" || parsedRange[1] != "127") {
+		rng = fmt.Sprintf(" %s:%s", parsedRange[0], parsedRange[1])
+	}
 	if volumeType.Type == "kNoteVelocity" {
+		return fmt.Sprintf("velocity%s", rng)
+	} else if volumeType.Type == "kCC" {
+		return fmt.Sprintf("CC%s%s", volumeType.Param1, rng)
+	} else {
 		return fmt.Sprintf("velocity")
 	}
 }
 
 func FormatMidiEvents(actions []doricolib.SwitchAction) string {
-
+	var result []string
+	for _, action := range actions {
+		switch action.Type {
+		case "kKeySwitch":
+			// TODO: convert to notes?
+			if action.Param2 != "" && action.Param2 != "127" {
+				result = append(result, fmt.Sprintf("KS%s=%s", action.Param1, action.Param2))
+			} else {
+				result = append(result, fmt.Sprintf("KS%s", action.Param1))
+			}
+		case "kProgramChange":
+			result = append(result, fmt.Sprintf("PC%s", action.Param1))
+		case "kControlChange":
+			result = append(result, fmt.Sprintf("CC%s=%s", action.Param1, action.Param2))
+		}
+	}
+	return strings.Join(result, ", ")
 }
 
 func CanonicalizeTechniqueString(tids string) string {
